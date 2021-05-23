dae2f30e7421e5ae4b0e6885be2da9f04f15bd15
Yanglin Zhao <yz1876@gmail.com>
2021-05-23 11:41:53 -0400

    Display an image

--- src/main.js
+++ src/main.js
@@ -1,6 +1,51 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+function documentReady(fn) {
+  if (document.readyState != "loading") {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+function setupCanvas() {
+  const canvas = document.createElement("canvas");
+  canvas.id = "app-image-mount";
+  document.getElementById("app").appendChild(canvas);
+  const convasContext = canvas.getContext("2d");
+
+  const img = new Image();
+  img.crossOrigin = "Anonymous";
+  img.src = "https://source.unsplash.com/collection/190727/800x450";
+
+  img.onload = function () {
+    convasContext.drawImage(
+      img,
+      0,
+      0,
+      img.width,
+      img.height,
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    );
+
+    const imageData = convasContext.getImageData(
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    );
+    console.log(imageData);
+  };
+  img.onerror = function (e) {
+    console.log("error", e);
+  };
+}
+
+function setup() {
+  setupCanvas();
+}
+
+documentReady(setup);

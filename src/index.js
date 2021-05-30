bc540aa9f78cb5118f3eb3c1347a4474964077b7
Yanglin Zhao <yz1876@gmail.com>
2021-05-30 13:58:46 -0400

    Disable HMR

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-import("./pkg").then((module) => {
-  module.greet("foo");
-});
+import React from "react";
+import ReactDOM from "react-dom";
+
+if (module.hot) {
+  // Disable HMR in development
+  module.hot.dispose();
+}
+
+function loadWasm() {
+  import("./pkg")
+    .then((module) => {
+      module.greet("foo");
+    })
+    .catch(console.error);
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Content Aware Image Resizer</h1>
+      <canvas></canvas>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));

52a82a06282300bc5d1d45631c861732c3212072
Yanglin Zhao <yz1876@gmail.com>
2021-05-30 14:10:55 -0400

    Load image

--- src/resizer.js
+++ src/resizer.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 function loadWasm() {
   import("./pkg")
     .then((module) => {
@@ -6,15 +8,52 @@ function loadWasm() {
     .catch(console.error);
 }
 
+function loadImage(canvas) {
+  const ctx = canvas.getContext("2d");
+
+  const img = new Image();
+  img.crossOrigin = "Anonymous";
+  img.src = "https://source.unsplash.com/collection/190727/800x450";
+
+  img.onload = function () {
+    ctx.drawImage(
+      img,
+      0,
+      0,
+      img.width,
+      img.height,
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    );
+
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    console.log(imageData);
+  };
+  img.onerror = function (e) {
+    console.log("error", e);
+  };
+}
+
+function handleResize(canvas) {
+  // TODO: implement this
+}
+
 function Resizer() {
+  useEffect(() => {
+    const canvas = document.getElementById("app-canvas");
+    loadImage(canvas);
+  }, []);
+
   function handleResize() {
     const canvas = document.getElementById("app-canvas");
-    const ctx = canvas.getContext("2d");
   }
 
   return (
     <div>
       <button onClick={handleResize}>Click me!</button>
+      <hr />
       <div>
         <canvas id="app-canvas"></canvas>
       </div>

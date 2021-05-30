aee74e1c519ac381ca4b1273d23a3312b1048913
Yanglin Zhao <yz1876@gmail.com>
2021-05-30 15:44:47 -0400

    Run format

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use wasm_bindgen::prelude::*;
-use web_sys::{CanvasRenderingContext2d};
+use web_sys::CanvasRenderingContext2d;
 
 mod carver;
 
 #[wasm_bindgen]
 pub fn resize(ctx: &CanvasRenderingContext2d) -> Result<(), JsValue> {
-    return carver::resize(ctx)
+    return carver::resize(ctx);
 }

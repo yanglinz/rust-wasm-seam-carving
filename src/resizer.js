625201a8b9a216201039c3fc5157a67296b033f1
Yanglin Zhao <yz1876@gmail.com>
2021-06-22 23:09:58 -0400

    Use new demo image (#12)
    * Use promise to load image
    
    * Prettier ignore
    
    * Use react refs
    
    * Use a slightly smaller image
    
    * Set a dynamic canvas style

--- src/resizer.js
+++ src/resizer.js
@@ -1,77 +1,96 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
-function loadImage(canvas) {
-  const ctx = canvas.getContext("2d");
+const DEMO_IMAGE = "https://source.unsplash.com/YJjm6XD6zF4/800x450";
 
+function loadImage(url) {
   const img = new Image();
   img.crossOrigin = "Anonymous";
-  img.src = "https://source.unsplash.com/collection/190727/800x450";
+  img.src = url;
 
-  img.onload = function () {
-    ctx.drawImage(
-      img,
-      0,
-      0,
-      img.width,
-      img.height,
-      0,
-      0,
-      canvas.width,
-      canvas.height
-    );
-
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    console.log(imageData);
-  };
-  img.onerror = function (e) {
-    console.log("error", e);
-  };
+  return new Promise((resolve, reject) => {
+    img.onload = function () {
+      resolve(img);
+    };
+    img.onerror = function (e) {
+      reject(e);
+    };
+  });
 }
 
-function carverRedize(canvas) {
+function loadCanvasImage(canvasElements) {
+  const { original, resized } = canvasElements;
+
+  loadImage(DEMO_IMAGE).then((img) => {
+    // TODO: Think of high DPI screens / scale down heuristics
+    original.width = img.width;
+    original.height = img.height;
+    resized.width = img.width;
+    resized.height = img.height;
+
+    const ctxOriginal = original.getContext("2d");
+    // prettier-ignore
+    ctxOriginal.drawImage(img, 0, 0, img.width, img.height, 0, 0, original.width, original.height);
+    // prettier-ignore
+    const imageData = ctxOriginal.getImageData(0, 0, original.width, original.height);
+    console.log(imageData);
+  });
+}
+
+function carverRedize(canvasElements) {
+  const { original, resized } = canvasElements;
+
   function wasmResize(...args) {
     import("./pkg")
       .then((module) => module.resize(...args))
       .catch(console.error);
   }
 
-  const ctx = canvas.getContext("2d");
-  const canvasTarget = document.getElementById("test-canvas");
-  const ctxTarget = canvasTarget.getContext("2d");
+  const ctxOrignal = original.getContext("2d");
+  const ctxResized = resized.getContext("2d");
 
   wasmResize(
-    ctx,
-    ctxTarget,
-    canvas.width,
-    canvas.height,
-    canvas.width - 50,
-    canvas.height
+    ctxOrignal,
+    ctxResized,
+    resized.width,
+    resized.height,
+    resized.width - 50,
+    resized.height
   );
 }
 
-function Resizer() {
-  useEffect(() => {
-    const canvas = document.getElementById("app-canvas");
-    loadImage(canvas);
-  }, []);
+function getCanvas() {}
 
-  function handleResize() {
-    const canvas = document.getElementById("app-canvas");
-    carverRedize(canvas);
+function Resizer() {
+  const canvasOriginal = useRef(null);
+  const canvasResized = useRef(null);
+
+  function getCanvasElements() {
+    return {
+      original: canvasOriginal.current,
+      resized: canvasResized.current,
+    };
   }
 
+  useEffect(() => {
+    if (canvasOriginal && canvasResized) {
+      loadCanvasImage(getCanvasElements());
+    }
+  }, [canvasOriginal, canvasResized]);
+
+  function handleResize() {
+    carverRedize(getCanvasElements());
+  }
+
+  const canvasStyle = { background: "rgba(0, 0, 0, 0.05)" };
   return (
     <div>
       <button onClick={handleResize}>Click me!</button>
       <hr />
       <div>
-        <canvas id="app-canvas"></canvas>
+        <canvas ref={canvasOriginal} style={canvasStyle}></canvas>
       </div>
       <div>
-        <canvas
-          id="test-canvas"
-          style={{ width: 300, height: 150, background: "#ddd" }}
-        ></canvas>
+        <canvas ref={canvasResized} style={canvasStyle}></canvas>
       </div>
     </div>
   );

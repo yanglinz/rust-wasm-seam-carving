8d7cc378b501a2b842d5956bed5810f59d167416
Yanglin Zhao <yz1876@gmail.com>
2021-07-10 00:14:05 -0400

    Use craco instead of react-app-rewired

--- craco.config.js
+++ craco.config.js
@@ -1,26 +1,27 @@
-const { addBeforeLoader, loaderByName } = require("@craco/craco");
+const path = require("path");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      const wasmExtensionRegExp = /\.wasm$/;
       webpackConfig.resolve.extensions.push(".wasm");
 
       webpackConfig.module.rules.forEach((rule) => {
         (rule.oneOf || []).forEach((oneOf) => {
           if (oneOf.loader && oneOf.loader.indexOf("file-loader") >= 0) {
-            oneOf.exclude.push(wasmExtensionRegExp);
+            // Make file-loader ignore WASM files
+            oneOf.exclude.push(/\.wasm$/);
           }
         });
       });
 
-      const wasmLoader = {
-        test: /\.wasm$/,
-        exclude: /node_modules/,
-        loaders: ["wasm-loader"],
-      };
-
-      addBeforeLoader(webpackConfig, loaderByName("file-loader"), wasmLoader);
+      webpackConfig.plugins = (webpackConfig.plugins || []).concat([
+        new WasmPackPlugin({
+          crateDirectory: path.resolve(__dirname, "src"),
+          extraArgs: "--no-typescript",
+          outDir: path.resolve(__dirname, "src/pkg"),
+        }),
+      ]);
 
       return webpackConfig;
     },

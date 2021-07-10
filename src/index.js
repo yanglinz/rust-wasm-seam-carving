530bed5256e6929fa591f584f7a9460b27267c25
Yanglin Zhao <yz1876@gmail.com>
2021-07-10 00:24:33 -0400

    Setup tailwind

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Resizer from "./resizer";
 
+import "./index.css";
+
 if (module.hot) {
   // Disable HMR in development
   module.hot.decline();

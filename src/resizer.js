0e34c9a4350f7f6db39cfdeb8c635a48ccc54ab7
Yanglin Zhao <yz1876@gmail.com>
2021-07-15 22:26:50 -0400

    Remove the outline of resizer

--- src/resizer.js
+++ src/resizer.js
@@ -1,62 +1,8 @@
 import { useEffect, useRef } from "react";
 
-const DEMO_IMAGE = "https://source.unsplash.com/yRjLihK35Yw/800x450";
+import { resizeImage } from "./lib";
 
-function loadImage(url) {
-  const img = new Image();
-  img.crossOrigin = "Anonymous";
-  img.src = url;
-
-  return new Promise((resolve, reject) => {
-    img.onload = function () {
-      resolve(img);
-    };
-    img.onerror = function (e) {
-      reject(e);
-    };
-  });
-}
-
-function loadCanvasImage(canvasElements) {
-  const { original, resized } = canvasElements;
-
-  loadImage(DEMO_IMAGE).then((img) => {
-    // TODO: Think of high DPI screens / scale down heuristics
-    original.width = img.width;
-    original.height = img.height;
-    resized.width = img.width;
-    resized.height = img.height;
-
-    const ctxOriginal = original.getContext("2d");
-    // prettier-ignore
-    ctxOriginal.drawImage(img, 0, 0, img.width, img.height, 0, 0, original.width, original.height);
-    // prettier-ignore
-    const imageData = ctxOriginal.getImageData(0, 0, original.width, original.height);
-    console.log(imageData);
-  });
-}
-
-function carverRedize(canvasElements) {
-  const { original, resized } = canvasElements;
-
-  function wasmResize(...args) {
-    import("./pkg")
-      .then((module) => module.resize(...args))
-      .catch(console.error);
-  }
-
-  const ctxOrignal = original.getContext("2d");
-  const ctxResized = resized.getContext("2d");
-
-  wasmResize(
-    ctxOrignal,
-    ctxResized,
-    resized.width,
-    resized.height,
-    resized.width - 50,
-    resized.height
-  );
-}
+// const DEMO_IMAGE = "https://source.unsplash.com/yRjLihK35Yw/800x450";
 
 function Resizer() {
   const canvasOriginal = useRef(null);
@@ -69,21 +15,18 @@ function Resizer() {
     };
   }
 
-  useEffect(() => {
-    if (canvasOriginal && canvasResized) {
-      loadCanvasImage(getCanvasElements());
-    }
-  }, [canvasOriginal, canvasResized]);
-
   function handleResize() {
-    carverRedize(getCanvasElements());
+    // First copy the image as-is to the target canvas
+    //
+    const currentWidth = "";
+    const currentHeight = "";
+    console.log(currentWidth, currentHeight);
+    // carverRedize(getCanvasElements());
   }
 
   const canvasStyle = { background: "rgba(0, 0, 0, 0.05)" };
   return (
     <div>
-      <button onClick={handleResize}>Click me!</button>
-      <hr />
       <div>
         <canvas ref={canvasOriginal} style={canvasStyle}></canvas>
       </div>

87e796d3fd640849e2be078addfe9a908e184ec3
Yanglin Zhao <yz1876@gmail.com>
2021-07-25 17:59:46 -0400

    Create styled portal element

--- src/components/ImageSelect.js
+++ src/components/ImageSelect.js
@@ -10,31 +10,77 @@ function getModalTargetEl(id = "dialog-mount") {
   return el;
 }
 
-function DialogInner(props) {
+function DialogPortal(props) {
   return ReactDOM.createPortal(props.children, getModalTargetEl());
 }
 
 function Dialog(props) {
   return (
-    <DialogInner>
+    <DialogPortal>
       <div
         className="fixed z-10 inset-0 overflow-y-auto"
-        aria-labelledby="modal-title"
         role="dialog"
         aria-modal="true"
       >
-        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-          {props.children}
+        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <div
+            className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+            aria-hidden="true"
+          ></div>
+
+          <span
+            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+
+          <div className="inline-block align-bottom bg-white rounded-sm text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+            {props.children}
+          </div>
         </div>
       </div>
-    </DialogInner>
+    </DialogPortal>
   );
 }
 
 function ImageSelect() {
+  const demoImages = [
+    "https://source.unsplash.com/random/200x150",
+    "https://source.unsplash.com/random/200x150",
+    "https://source.unsplash.com/random/200x150",
+    "https://source.unsplash.com/random/200x150",
+    "https://source.unsplash.com/random/200x150",
+    "https://source.unsplash.com/random/200x150",
+    "https://source.unsplash.com/random/200x150",
+    "https://source.unsplash.com/random/200x150",
+    "https://source.unsplash.com/random/200x150",
+  ];
+
   return (
     <Dialog>
-      <h1>Foo Bar</h1>
+      <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+        <div className="flex flex-col content-center">
+          <h3 className="my-3 text-lg leading-6 font-medium text-gray-900">
+            Select Image
+          </h3>
+
+          <div className="overflow-hidden grid grid-cols-3 gap-1">
+            {demoImages.map((url) => (
+              <img className="inline-block h-20 w-20" src={url}></img>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+        <button
+          type="button"
+          className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+        >
+          Cancel
+        </button>
+      </div>
     </Dialog>
   );
 }
